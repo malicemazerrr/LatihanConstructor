c088fc518238cad448b06297fa9b5ec5337bbb01
malicemazerrr <rwizeyeon1@gmail.com>
2026-04-28 14:48:01 +0700

    Add files via upload

---
+++ LatihanConstructor/LatihanConstructor/Mahasiswa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanConstructor
+{
+    internal class Mahasiswa
+    {
+        public Mahasiswa()
+        {
+
+        }
+
+        public Mahasiswa(string nim, string nama)
+        {
+            Nim = nim;
+            Nama = nama;
+        }
+        public string Nim { get; set; }
+        public string Nama { get; set;}
+    }
+}
---
+++ LatihanConstructor/LatihanConstructor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa mhs1=new Mahasiswa();
+            CetakHasil(mhs1);
+
+            Console.WriteLine();
+
+            Mahasiswa mhs2=new Mahasiswa("12345", "Paijo");
+            CetakHasil(mhs2);
+
+            Console.ReadKey();
+        }
+
+        static void CetakHasil(Mahasiswa mhs)
+        {
+            Console.WriteLine("Nim: {0}", mhs.Nim);
+            Console.WriteLine("Nama: {0}", mhs.Nama);
+        }
+    }
+}
